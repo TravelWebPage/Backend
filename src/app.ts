cedc80214255a4d50b132d85208aebd2d3b92873
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-04-30 12:00:21 +0900

    [CREATE] 계정 생성
    회원가입 구현

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,18 @@ import router from "./router/router";
 
 const app = express();
 
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host:process.env.DB_HOST,
+  port:process.env.DB_PORT,
+  user:process.env.DB_USER ,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_DATABASE
+});
+
+connection.connect();
+
 app.get("/", (request: Request, response: Response, next: NextFunction) => {
   response.send("hello");
 });
